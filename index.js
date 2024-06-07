1af7d6484a54d15b1c7d8965fdbee5aa69562fe5
YumnaShams0623 <yumniijan26471@gmial.com>
2024-06-07 17:58:47 +0500

    If statement Examples

---
+++ index.js
@@ -0,0 +1,93 @@
+
+    //BRT CARD 
+//var brtcard_amount = prompt("Enter Your BRT-CARD AMOUNT")
+//if (brtcard_amount >= 55) {
+// alert("WELLCOME TO ZUU PESHAWAR");
+//}
+// else  {
+//alert("Pleasee recharge your account");
+//}
+
+//----------------------------------------------------
+
+    //BURGER PRIZE
+//var burger_prize = "100/-"
+//var pocket = prompt ("How many Rs in your Pocket")
+//if (pocket >= 100) {
+//   alert(" Burger lelo")
+//} else {
+//  alert("aj guzara kr le tery pass itny pesy ni hn !")
+//}
+
+//----------------------------------------------------
+    //BARISH
+//var badal = prompt("Badal hn ?")
+//if (badal === "hn") {
+//  alert("Aj Barish hogi 🌧");  
+//} else  {
+//  alert("Aj Barish Nhi Hogi");
+//} 
+
+//----------------------------------------------------
+    //TRAFIC LIGHT
+//var light = prompt("Trafic light ka color kia h?")
+//    if (light === "red")       {
+//       alert("STOP🚷")
+//}           
+//   if (light === "green") {
+//      alert("Go !")
+//}
+//   if (light === "yellow") {
+//       alert("Ready !")
+//}
+
+//----------------------------------------------------
+
+    //Odd & Even
+//var num = prompt("Enter a number. I will tell u it is ODD or EVEN")
+//if (num == 2 ){
+//    alert( "Its an Even Number");
+//} else if ( num == 3) {
+//    alert ("Its an Odd num");
+//}
+
+//-------------------------------------------------------
+
+//Fruit Rate
+//var fruits = prompt("Apple & mango avaible")
+//if(fruits == "apple"){
+//   alert(apple)
+//}else if(fruits == "mango"){
+//   alert(mango)
+//}
+//
+ // var apple  = 500;
+ // var mango = 500;
+ // var total_bill = apple + mango;
+ // console.log(total_bill);
+ //--------------------------------------------------------
+
+
+ //DISCOUNT
+// var discount = prompt("No. of items")
+ //if(discount >= 10){
+ //   alert("20% DISCOUNT")
+ //} else if(discount >= 5){
+ //   alert("10% DISCOUNT")
+ //}else if (discount <=9){
+ //   alert("10% DISCOUNT")
+ //} else if  (discount <= 5) {
+  //  alert("00% DISCOUNT")
+ //}
+//-----------------------------------------------------------
+
+//BANK BALANCE
+
+var bank_balance = prompt("Apna bank balanace batayn !")
+var withdraw = prompt ("withdraw amount enter krn !")
+if(bank_balance >= withdraw){
+    alert ("apko pesy miln gy")
+}else{
+    alert("itny pesy daly nahi jitny mang rhy ho yr !")
+}
+
